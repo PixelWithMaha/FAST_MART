f6be451f0f1bc059bb10246eac23c5a12a749929
PixelWithMaha <maahamrizwan0@gmail.com>
2026-04-30 23:20:28 +0500

    chat function modifications

--- app/src/main/java/com/example/assignment3/data/model/ChatMessage.java
+++ app/src/main/java/com/example/assignment3/data/model/ChatMessage.java
@@ -1,4 +1,15 @@
 package com.example.assignment3.data.model;
 
 public class ChatMessage {
+    public String senderId, receiverId, messageText;
+    public long timestamp;
+
+    public ChatMessage() {} // Required for Firebase
+
+    public ChatMessage(String senderId, String receiverId, String messageText, long timestamp) {
+        this.senderId = senderId;
+        this.receiverId = receiverId;
+        this.messageText = messageText;
+        this.timestamp = timestamp;
+    }
 }
